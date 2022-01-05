20a4ff797f2c8709e69adc2d51d65399ac1f7787
Ben Best <ben@ecoquants.com>
2022-01-05 08:55:36 -0800

    + lab2_sdm-regress

--- _functions.R
+++ _functions.R
@@ -1,12 +1,12 @@
 librarian::shelf(
-  dplyr, DT, glue, readr, stringr, tidyr)
+  dplyr, DT, glue, googlesheets4, readr, stringr, tidyr)
 options(readr.show_col_types = F)
 
 # edit: * [schedule | eds232-ml - Google Sheets](https://docs.google.com/spreadsheets/d/1lolwZ2CNAtUkTWPauyPEe_oVMknqS2yVvfnB1fiErzQ/edit#gid=0)
-sched_csv <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQT4PYzXahIpCV2-PXR_97niUglLuxKuxhhRKTw5tWMXX7HLyhnL23BRvP3V7sTEd4Shd60Q5ff-g3l/pub?gid=0&single=true&output=csv"
+sched_gsheet <- "https://docs.google.com/spreadsheets/d/1lolwZ2CNAtUkTWPauyPEe_oVMknqS2yVvfnB1fiErzQ/edit"
 
 get_sched <- function(){
-  d_sched <- readr::read_csv(sched_csv) %>%
+  d_sched <- googlesheets4::read_sheet(sched_gsheet) %>%
     tidyr::fill(Week, Module)
   names(d_sched) <- names(d_sched) %>% stringr::str_replace("\n", " ")
   d_sched
